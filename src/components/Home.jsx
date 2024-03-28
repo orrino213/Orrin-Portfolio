18a0006125473470dc241372cfd0c6aa7ca67ed0
orrino213 <orrinohanian13@gmail.com>
2024-03-28 14:34:09 -0700

    new commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,18 @@ import "./home.css";
 
 function Home() {
   return (
-    <div>
-      <p>eyyyy its edddggghhminnn</p>
+    <h1>
+      <p>eyyyy its me deggggggggg</p>
+      <h1>yo yo</h1>
+      <h1>yo yo</h1>
+      <h1>yo yo</h1>
+      <h1>yo yo</h1>
       <img
         src="https://t4.ftcdn.net/jpg/01/08/65/91/360_F_108659197_qlPMbZOAsnDbsAsq5t7vfq0mA2zOxZ5d.jpg"
         alt="placeholder"
         className="home"
       ></img>
-    </div>
+    </h1>
   );
 }
 
