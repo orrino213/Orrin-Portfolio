97f7762f0ae21c2a45a16ac26f9d259583c85589
orrino213 <orrinohanian13@gmail.com>
2024-03-28 20:39:23 -0700

    another one

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,6 @@ function Home() {
       <h1>yo yo</h1>
       <h1>yo yo</h1>
       <h1>yo yo</h1>
-      <h1>yo yo</h1>
       <img
         src="https://t4.ftcdn.net/jpg/01/08/65/91/360_F_108659197_qlPMbZOAsnDbsAsq5t7vfq0mA2zOxZ5d.jpg"
         alt="placeholder"
