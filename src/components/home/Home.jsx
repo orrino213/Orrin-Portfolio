1c3cee9d346df583a7da1f6bc58e0a024e817118
orrino213 <orrinohanian13@gmail.com>
2024-04-09 12:48:43 -0700

    another

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,11 @@ function Home() {
   return (
     <div>
       <h1 className="title">ORBEL THE LOAN OFFICER</h1>
+      <h5 className="mission">
+        "My mission is to empower individuals & families with personalized
+        mortgage solutions because homeownership is the catalyst for freedom,
+        stability and wealth!"
+      </h5>
       <MyCarousel />
       <AboutUs />
     </div>
